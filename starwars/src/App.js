321594cbbab637ee41d4e991ad2cc83153a3e071
Brendon <bbrendonshi@gmail.com>
2018-11-13 10:43:15 +1100

    added basic functionality for retrieving from SWAPI

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+ 
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(){
+        super();
+        this.state = {
+            // array which stores items from SWAPI 
+            items: [],
+            // boolean variable
+            isLoaded: false,
+        }
+    }
+    // runs after render method, and updates render to output resources
+    componentDidMount(){
+        fetch('https://swapi.co/api/planets/')
+            // convert to json format
+            .then(response => response.json())
+            // arrow functions is used to not lose
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    items: json,
+                })
+            });
+    }
+
+    // render method responsible for producing output
+    render() {
+
+        // create variable to access items in state
+        var { isLoaded, items } = this.state;
+
+        if(!isLoaded){
+            return(
+                <div> Data is loading </div>
+            );
+        }
+        else{
+            return (
+              // create unordered list
+              // use map function which creates new array, which loops all objects from API 
+                <div className="App">
+                    <ul>
+                        {items.results.map(item => (
+                            <li key = {item.name}>
+                                {item.name}
+                            </li>
+                          ))}
+                    </ul>
+                    Data has been loaded.
+                </div>
+            );
+        }
+
+
+    }
 }
 
 export default App;
