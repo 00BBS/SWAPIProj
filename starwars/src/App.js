3cba6011146216c45475a38cdb1dff6e4ff60b6a
Brendon <bbrendonshi@gmail.com>
2018-11-15 11:38:34 +1100

    completed table feature with all planets

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,77 +2,59 @@ import React, { Component } from 'react';
 import './App.css'
 import Table1 from './Table1';
 
-var list = []
-
-
 class App extends Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
-            // array which stores items from SWAPI 
-            items: [],
             // boolean variable
             isLoaded: false,
+            items: [],
         }
+        // this.fetchPlanets = this.fetchPlanets.bind(this);
     }
 
     // runs after render method, and updates render to output resources
     componentDidMount(){
-        fetchPlanets();
-        fetch('https://swapi.co/api/planets/')
-            // convert to json format
-            .then(response => response.json())
-            .then(json => {
-                console.log(json)
+        this.fetchPlanets();
+    }
+
+    fetchPlanets(){
+        var pagesRequired = 7;
+        var base = "https://swapi.co/api/planets/?page=";
+        for(var i = 1; i <= pagesRequired; i++){
+            fetch(base + i).then(res => res.json()).then(json =>{
+                var connected = this.state.items.concat(json.results);
                 this.setState({
-                    isLoaded: true,
-                    items: json,
+                    items: connected
                 })
-            });
+            }).then(final => {
+                this.setState({
+                    isLoaded: true
+                })
+            })
+        }
     }
 
     // render method responsible for producing output
     render() {
         // create variable to access items in state
         var { 
-                isLoaded, 
-                items       } = this.state;
-
+                isLoaded,
+                items,  } = this.state;
         if(!isLoaded){
             return(
                 <div> Data is loading </div>
             );
         }
         else{
+            console.log(items)
             return (
                 <div className="App">
-                    <p className="Table-header">Basic Table</p>
-                    <Table1 data={items.results}/>
+                    <p className="Table-header">The Planets of Star Wars</p>
+                    <Table1 data={items}/>
                 </div>
             );
         }
     }
-}
-
-function fetchPlanets(){
-    var pagesRequired = 7;
-    const apiPromises = [];
-    var base = "https://swapi.co/api/planets/?page=";
-    for(var i = pagesRequired; i > 0; i--){
-        apiPromises.push(fetch(base + i));
-        console.log(base + i)
-    }
-    Promise.all(apiPromises).then(responses => {
-        const processedResponses = [];
-        // map each response into an array
-        responses.map(response => {
-            processedResponses.push(response)
-        })
-        processedResponses[1].json().then(data => {
-            console.log(data.results[2])
-        })
-        console.log(list)
-    })
-
-}
+}        
 export default App;
